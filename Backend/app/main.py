3ec1c9b340782ef558faa7570349de46f036629a
we9d <supasinq@gmail.com>
2026-09-06 12:32:33 +0700

    feat: implement main application entry point and CRUD operations for posts router

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -4,11 +4,13 @@ from app.core.security import get_current_user
 from app.db.supabase import supabase
 from app.routes.profile import router as profile_router
 from app.routes.auth import router as auth_router
+from app.routes.posts import router as posts_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(profile_router)
+app.include_router(posts_router)
 
 @app.get("/")
 def read_root():
